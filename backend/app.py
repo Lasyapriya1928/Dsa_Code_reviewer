1603854f0634e31d11d533f1dca38d9022e22228
Lasya priya <lasyapriya1928@gmail.com>
2026-02-24 23:21:11 +0530

    simplify cors

--- backend/app.py
+++ backend/app.py
@@ -30,7 +30,7 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origin_regex="https://.*\.vercel\.app",
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
